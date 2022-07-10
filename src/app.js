d067c1c45c87669af13b8fb3925cdcb79affe6a4
crisitan lair <crisitanlair@gmail.com>
2022-07-10 18:13:42 -0300

    actualizando git

--- src/app.js
+++ src/app.js
@@ -10,17 +10,17 @@ const server = express();
 
 server.name = 'API';
 
-server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
-server.use(bodyParser.json({ limit: '50mb' }));
-server.use(cookieParser());
-server.use(morgan('dev'));
-server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-  next();
-});
+ server.use(function(req, res, next) {
+  let allowedOrigins = ['https://poke-api-henry.vercel.app', 'http://localhost:3000', 'https://api-pokemon-deploy.herokuapp.com/pokemons'];
+  let origin = req.headers.origin;
+  if(allowedOrigins.indexOf(origin) > -1){
+       res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+  res.header('Access-Control-Allow-Methods', 'GET, POST, DELETE, PUT, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', true);
+  return next();
+}); 
 
 server.use('/', routes);
 
