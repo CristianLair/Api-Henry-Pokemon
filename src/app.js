c736461c0713ddcb783d384c6265633c3c4866ad
crisitan lair <crisitanlair@gmail.com>
2022-07-15 12:12:08 -0300

    otra prueba cors

--- src/app.js
+++ src/app.js
@@ -10,16 +10,19 @@ const server = express();
 
 server.name = 'API';
 
-server.use(cors());
-server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-  next();
-});
+server.use(
+  cors({
+    credentials: true,
+    origin:  ['https://poke-api-henry.vercel.app', 'https://api-pokemon-deploy.herokuapp.com/types', 'http://localhost:3000', 'https://api-pokemon-deploy.herokuapp.com/pokemons', 'https://git.heroku.com/henry-proyecto-nft.git'],
+    methods: ["GET", "POST", "DELETE", "PUT", "OPTIONS"],
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "Access-Control-Allow-Origin",
+    ],
+  })
+
+)
 server.use(express.json())
 server.use('/', routes);
 
